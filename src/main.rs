e744efd455a9ea21a77aeb9ec494ee1533ad8eeb
Sigurd Holsen <sigurd.holsen@cognite.com>
2025-12-25 22:30:09 +0100

    formatting and group by in process

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use anyhow::Context;
 use clap::Parser;
+use itertools::Itertools;
 use rusqlite::{Connection, Result, params};
 use std::collections::HashMap;
-use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::{Path, PathBuf};
@@ -16,27 +17,56 @@ struct Args {
     folders: Vec<PathBuf>,
 
     #[arg(short, long)]
-    db: Option<PathBuf>,
+    db_path: Option<PathBuf>,
+
+    #[arg(short, long)]
+    exclude_globs: Vec<String>,
 }
 
 #[allow(dead_code)]
+#[derive(Clone, Debug)]
 struct FileRecord {
     path: String,
     size: i64,
     hash: Option<String>,
 }
 
+#[derive(Debug)]
+struct Config {
+    includes: Vec<PathBuf>,
+    excludes: Vec<glob::Pattern>,
+}
 
-fn add_folder(
-    conn: &mut Connection,
-    path: &Path,
-) -> anyhow::Result<()> {
+impl Config {
+    fn is_included(&self, path: &Path) -> bool {
+        let mut is_included = false;
+        for inc in self.includes.iter() {
+            if path.starts_with(inc) {
+                is_included = true;
+                break;
+            }
+        }
+        if !is_included {
+            return false;
+        }
+
+        for exc in self.excludes.iter() {
+            if exc.matches_path(path) {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+fn fetch(conn: &mut Connection) -> Vec<FileRecord> {
     let mut stmt = conn
-        .prepare("select path, size, hash from files where path like (?1 || '%')")
+        .prepare("select path, size, hash from files")
         .expect("select all files");
 
     let existing_files: Vec<FileRecord> = stmt
-        .query_map([path.canonicalize().unwrap().to_str().unwrap()], |row| {
+        .query_map([], |row| {
             Ok(FileRecord {
                 path: row.get(0).expect("Should get path"),
                 size: row.get(1).expect("should get size"),
@@ -46,26 +76,33 @@ fn add_folder(
         .unwrap()
         .flatten()
         .collect();
-    let mut map = HashMap::new();
-    for record in existing_files.into_iter() {
-        map.insert(record.path.clone(), record);
-    }
-    drop(stmt);
+    return existing_files;
+}
+
+fn add_folder(conn: &mut Connection, config: &Config, path: &Path) -> anyhow::Result<()> {
+    let map = {
+        let existing_files = fetch(conn);
+        let mut map = HashMap::new();
+        for record in existing_files.into_iter() {
+            map.insert(record.path.clone(), record);
+        }
+        map
+    };
 
     info!("Found {} files", map.len());
-
 
     let mut tx = conn.transaction()?;
     let mut i = 0;
 
     for entry in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_file() {
-            let path = entry.path().display().to_string();
-            if !map.contains_key(&path) {
+            let path = entry.path();
+            let path_name = path.display().to_string();
+            if !map.contains_key(&path_name) && config.is_included(path) {
                 let size = entry.metadata().map(|m| m.len()).unwrap_or(0);
                 tx.execute(
                     "INSERT INTO files (path, size) VALUES (?1, ?2)",
-                    params![path, size as i64],
+                    params![path_name, size as i64],
                 )?;
                 i += 1;
 
@@ -85,8 +122,26 @@ fn add_folder(
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
-    let path = args.db.unwrap_or(PathBuf::from("files.db"));
-    let mut conn = Connection::open(&path)?;
+    let mut conn = match args.db_path {
+        Some(path) => Connection::open(&path)?,
+        None => Connection::open_in_memory()?,
+    };
+    let config = Config {
+        excludes: args
+            .exclude_globs
+            .iter()
+            .map(|x| glob::Pattern::new(x).expect("Valid pattern"))
+            .collect(),
+        includes: args
+            .folders
+            .iter()
+            .map(|x| {
+                x.canonicalize()
+                    .context(format!("Could not find file {x:?}"))
+            })
+            .collect::<Result<Vec<PathBuf>, anyhow::Error>>()
+            .unwrap(),
+    };
 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS files (
@@ -97,37 +152,39 @@ fn main() -> Result<()> {
         [],
     )?;
 
-    let folders: Vec<_> = args
-        .folders
-        .iter()
-        .map(|x| x.canonicalize().expect("Canonicalize"))
-        .collect();
-
-    for dir in folders {
-        add_folder(&mut conn, &dir).expect("Failed to add folder");
+    for dir in config.includes.iter() {
+        add_folder(&mut conn, &config, dir).expect("Failed to add folder");
     }
 
-    info!("Finding duplicates by size");
-    let mut stmt = conn.prepare(
-        "SELECT path, size, hash FROM files WHERE size IN (
-            SELECT size FROM files GROUP BY size HAVING COUNT(*) > 1
-        )",
-    )?;
+    info!("Group by size");
+    let files: Vec<_> = fetch(&mut conn)
+        .into_iter()
+        .filter(|x| config.is_included(Path::new(&x.path)))
+        .sorted_by(|a, b| a.size.cmp(&b.size))
+        .collect();
 
-    let candidate_iter: Vec<_> = stmt.query_map([], |row| {
-        Ok(FileRecord {
-            path: row.get(0)?,
-            size: row.get(1)?,
-            hash: row.get(2).ok(),
-        })
-    })?.collect();
-    drop(stmt);
+    let mut to_check_hash: Vec<FileRecord> = vec![];
+
+    let mut prev = &files[0];
+    for file in files.iter().skip(1) {
+        if file.size == prev.size {
+            let prev_is_added = to_check_hash
+                .last()
+                .map(|x| x.path == prev.path)
+                .unwrap_or(false);
+            if !prev_is_added {
+                to_check_hash.push(prev.clone());
+            }
+            to_check_hash.push(file.clone());
+        }
+        prev = file;
+    }
 
     info!("Running md5sum on duplicates by size");
     let mut tx = conn.transaction()?;
     let mut i = 0;
-    for file_ref in candidate_iter {
-        let file_data = file_ref?;
+    for file_ref in to_check_hash {
+        let file_data = file_ref;
         if file_data.hash.is_some() {
             continue;
         }
@@ -147,26 +204,41 @@ fn main() -> Result<()> {
     }
     tx.commit()?;
 
-    println!("\n--- Duplicate Files Found ---");
-    let mut stmt = conn.prepare(
-        "SELECT path, size, hash FROM files WHERE hash IS NOT NULL AND hash IN (
-            SELECT hash FROM files WHERE hash IS NOT NULL GROUP BY hash HAVING COUNT(*) > 1
-        ) ORDER BY hash",
-    )?;
+    info!("Finding duplicates by hash");    
+    let files: Vec<_> = fetch(&mut conn)
+        .into_iter()
+        .filter(|x| config.is_included(Path::new(&x.path)))
+        .sorted_by(|a, b| a.size.cmp(&b.size))
+        .collect();
 
-    let mut rows = stmt.query([])?;
-    let mut prev = "".to_string();
-    while let Some(row) = rows.next()? {
-        let path: String = row.get(0)?;
-        let size: i64 = row.get(1)?;
-        let hash: String = row.get(2)?;
-        if size < 1000 { continue }
-        if hash != prev {
-            println!();
-            prev = hash.clone();
-            println!("\nNew file: {} with size {}", &prev, &size);
+    let mut duplicates: Vec<FileRecord> = vec![];
+
+    let mut prev = &files[0];
+    for file in files.iter().skip(1) {
+        if file.size == prev.size && file.hash == prev.hash {
+            let prev_is_added = duplicates
+                .last()
+                .map(|x| x.path == prev.path)
+                .unwrap_or(false);
+            if !prev_is_added {
+                duplicates.push(prev.clone());
+            }
+            duplicates.push(file.clone());
         }
-        println!("- {}", path);
+        prev = file;
+    }
+
+    let mut prev = "".to_string();
+    for duplicate in duplicates {
+        if duplicate.size < 1000 {
+            continue;
+        }
+        if duplicate.hash.clone().unwrap() != prev {
+            println!();
+            prev = duplicate.hash.unwrap().clone();
+            println!("\nNew file: {} with size {}", &prev, &duplicate.size);
+        }
+        println!("- {}", duplicate.path);
     }
 
     Ok(())
@@ -186,4 +258,16 @@ fn compute_md5(path: &str) -> io::Result<String> {
     }
 
     Ok(format!("{:x}", context.finalize()))
+}
+
+#[cfg(test)]
+mod test {
+    use std::path::Path;
+
+    #[test]
+    fn test_pattern() {
+        let pattern = glob::Pattern::new("**/test/**").unwrap();
+        let path = Path::new("a/b/c/test/a");
+        assert!(pattern.matches_path(path));
+    }
 }
